aa6b9149bd70536641580308180ac84af1bcc1f8
Raphael Guntersweiler <raphael@rgunti.ch>
2018-07-13 23:32:49 +0200

    Added live map to Map view

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,16 @@
 var map;
 var pin;
 
+var pins = {};
+var infoWindows = {};
+var selectedDevice = null;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('mapTarget'), {
-        center: { lat: 51.50722, lng: 0.1275 }, /* London */
+        center: { lat: 51, lng: 0 },
         zoom: 8
     });
+    init();
 }
 
 function setMapLocation(lat,lon) {
@@ -22,6 +27,89 @@ function setMapLocation(lat,lon) {
     map.panTo(pos);
 }
 
+function selectDevice(id) {
+    // Set active
+    $('#deviceList a').removeClass('active');
+    $('#deviceList a[data-id=' + id + ']').addClass('active');
+
+    selectedDevice = $(this).data('id');
+}
+
+function init() {
+    //setMapLocation(51.50722, 0.1275);
+    update(function() {
+        if (window.location.hash && window.location.hash.length > 1) {
+            selectDevice(window.location.hash.substr(1));
+        }
+        updateLoop();
+    });
+}
+
+function updateLoop() {
+    update(function() {
+        setTimeout(updateLoop, 2500);
+    });
+}
+
+function update(callback) {
+    $.ajax({
+        url: APP_BASE_URL + '/map/update.ajax',
+        method: 'GET'
+    }).done(function(obj) {
+        var points = obj.data;
+        for (var deviceId in points) {
+            var point = points[deviceId];
+            if (!point) continue;
+            var coord = new google.maps.LatLng(point.latitude, point.longitude);
+
+            var pin;
+            if (deviceId in pins) {
+                pin = pins[deviceId];
+                var infoWindow = infoWindows[deviceId];
+                pin.setPosition(coord);
+                infoWindow.setContent(getContentString(point));
+            } else {
+                pin = new google.maps.Marker({
+                    position: coord,
+                    map: map,
+                    title: 'Device #' + point.device_id,
+                    animation: google.maps.Animation.DROP
+                });
+                var infoWindow = new google.maps.InfoWindow({
+                    content: getContentString(point)
+                });
+
+                pin.addListener('click', function() {
+                    infoWindow.open(map, pin);
+                });
+
+                pins[deviceId] = pin;
+                infoWindows[deviceId] = infoWindow;
+            }
+
+            map.panTo(coord);
+        }
+
+        if (callback) callback();
+    }).fail(function() {
+        console.log(arguments);
+    });
+}
+
+function getContentString(point) {
+    return '<div class="content">'
+        + '<h3>Device #' + point.device_id + '</h3>'
+        + '<div id="bodyContent">'
+        + 'Latitude: ' + point.latitude + '<br>'
+        + 'Longitude: ' + point.longitude + '<br>'
+        + 'Last updated at: ' + point.updatedAt
+        + '</div>'
+        + '</div>';
+}
+
 $(document).ready(function() {
-    setMapLocation(51.50722, 0.1275);
+    $('#deviceList a').click(function() {
+        var self = $(this);
+        selectDevice(self.data('id'));
+    });
 });
--- routes/map.route.js
+++ routes/map.route.js
@@ -1,20 +1,72 @@
 const debug = require('debug')('PinMyPi:Routes:map');
 const config = require('config');
+const async = require('async');
 const HandleRender = require('../utils/handlebar-renderer');
 const auth = require('../auth');
 const AuthUtils = require('../auth/utils');
 const RouteUtils = require('../utils/route-utils');
 const ensureLoggedIn = require('../auth/utils').ensureLoggedIn;
 const Utils = require('../utils');
+const ApiUtils = require('../utils/api');
 
-const Device = require('../db/models').Device;
+const db = require('../db/models');
+const Device = db.Device;
+const Point = db.Point;
 
 const router = require('express').Router();
 
 router.get('/',
     ensureLoggedIn(),
     (req, res, next) => {
-        return HandleRender.render(res, 'map', 'Device Map');
+        Device.findAll({
+            where: { owner_id: req.user.id }
+        }).then((devices) => {
+            return HandleRender.render(res, 'map', 'Device Map', {
+                devices: devices
+            });
+        }).catch((err) => {
+            console.error(err);
+            next(err);
+        });
+    });
+
+router.get('/update.ajax',
+    ensureLoggedIn(),
+    (req, res, next) => {
+        Device.findAll({
+            where: { owner_id: req.user.id }
+        }).then((devices) => {
+            let devicePositions = {};
+            async.forEachOf(devices, (device, key, callback) => {
+                Point.findOne({
+                    where: { device_id: device.id },
+                    order: [['createdAt', 'DESC']]
+                }).then((point) => {
+                    if (point) {
+                        devicePositions[device.id] = point;
+                    } else {
+                        devicePositions[device.id] = null;
+                    }
+                    return callback();
+                }).catch((err) => {
+                    return callback(err);
+                });
+            }, (err) => {
+                if (err) {
+                    return ApiUtils.errorResponse(
+                        res, 
+                        'Failed to fetch current state', 
+                        null, 
+                        ApiUtils.HttpStatusCode.ServerError
+                    );
+                } else {
+                    return ApiUtils.dataResponse(res, devicePositions);
+                }
+            })
+        }).catch((err) => {
+            console.error(err);
+            next(err);
+        });
     });
 
 module.exports = {
